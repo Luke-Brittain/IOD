a8f000a1d78e06022318ce79a38c9c28d93e9ca5
KayaBread <kaya.bread@example.com>
2026-02-08 16:05:18 +0800

    ci: add eslint-plugin-vitest and use plugin:vitest/recommended in ESLint config

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -7,11 +7,14 @@ module.exports = {
     project: './tsconfig.json',
   },
   plugins: ['@typescript-eslint'],
-  extends: ['eslint:recommended', 'plugin:@typescript-eslint/recommended'],
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:vitest/recommended'
+  ],
   env: {
     node: true,
-    es2021: true,
-    vitest: true,
+    es2021: true
   },
   rules: {
     'no-unused-vars': 'off',
